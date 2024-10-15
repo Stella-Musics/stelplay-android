fd8c33b73d246f5777703fa186a98bc221f99543
sunghyun <pjhomg5530@naver.com>
2024-10-15 10:27:12 +0900

    :heavy_plus_sign: :: CI에 필요한 의존성 추가
    - Ktlint 의존성 추가
    - Detekt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,18 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.ktlint)
+    alias(libs.plugins.detekt)
+}
+
+allprojects {
+    afterEvaluate {
+        apply(plugin = "io.gitlab.arturbosch.detekt")
+
+        detekt {
+            toolVersion = "1.23.6"
+            buildUponDefaultConfig = true
+            config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        }
+    }
 }
