a427d5b317839cc3df000f89c64fc6f57c8db7d9
sunghyun <pjhomg5530@naver.com>
2024-11-04 16:32:31 +0900

    :heavy_plus_sign: :: baselineProfile 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,15 @@
 plugins {
     id("convention.android.application")
+    alias(libs.plugins.baselineprofile)
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.profileinstaller)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext)
     androidTestImplementation(libs.androidx.test.espresso)
+
+    "baselineProfile"(project(":baselineprofile"))
+    implementation(libs.androidx.profileinstaller)
 }
